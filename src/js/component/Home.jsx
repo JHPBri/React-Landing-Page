9293ae2c7af06a5373d16c4be14b161794cc7257
JHPBri <janghsuh@gmail.com>
2022-05-05 00:39:22 +0000

    test

---
+++ src/js/component/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./Card.jsx";
+
+//create your first component
+const Home = () => {
+	return (
+		<div>
+			<Card />
+		</div>
+	);
+};
+
+export default Home;
---
+++ src/js/component/Navbar.jsx
@@ -0,0 +1,12 @@
+
+
+//create your first component
+const Home = () => {
+	return (
+		<div>
+			
+		</div>
+	);
+};
+
+export default Home;
