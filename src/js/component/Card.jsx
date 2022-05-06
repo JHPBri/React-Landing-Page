ce29ae45e750a5e4884d864fc44bb23d0a09d550
JHPBri <janghsuh@gmail.com>
2022-05-06 22:43:43 +0000

    test

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -2,23 +2,82 @@ import React from "react";
 
 const Card = () => {
 	return (
-		<div className="card m-5">
-			<img
-				className="card-img-top"
-				src="https://github.com/4GeeksAcademy/react-tutorial-exercises/blob/master/.learn/assets/Dylan.png?raw=true"
-				alt="Card image cap"
-			/>
-			<div className="card-body">
-				<h5 className="card-title">hello</h5>
-				<p className="card-text">
-					Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an
-					American singer-songwriter.
-				</p>
-				<a
-					href="https://en.wikipedia.org/wiki/Bob_Dylan"
-					className="btn btn-primary">
-					Go to wikipedia
-				</a>
+		<div className="row">
+			<div className="card col-3">
+				<img
+					className="card-img-top"
+					src="https://github.com/4GeeksAcademy/react-tutorial-exercises/blob/master/.learn/assets/Dylan.png?raw=true"
+					alt="Card image cap"
+				/>
+				<div className="card-body">
+					<h5 className="card-title">hello</h5>
+					<p className="card-text">
+						Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is
+						an American singer-songwriter.
+					</p>
+					<a
+						href="https://en.wikipedia.org/wiki/Bob_Dylan"
+						className="btn btn-primary">
+						Go to wikipedia
+					</a>
+				</div>
+			</div>
+			<div className="card col-3">
+				<img
+					className="card-img-top"
+					src="https://github.com/4GeeksAcademy/react-tutorial-exercises/blob/master/.learn/assets/Dylan.png?raw=true"
+					alt="Card image cap"
+				/>
+				<div className="card-body">
+					<h5 className="card-title">hello</h5>
+					<p className="card-text">
+						Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is
+						an American singer-songwriter.
+					</p>
+					<a
+						href="https://en.wikipedia.org/wiki/Bob_Dylan"
+						className="btn btn-primary">
+						Go to wikipedia
+					</a>
+				</div>
+			</div>
+			<div className="card col-3">
+				<img
+					className="card-img-top"
+					src="https://github.com/4GeeksAcademy/react-tutorial-exercises/blob/master/.learn/assets/Dylan.png?raw=true"
+					alt="Card image cap"
+				/>
+				<div className="card-body">
+					<h5 className="card-title">hello</h5>
+					<p className="card-text">
+						Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is
+						an American singer-songwriter.
+					</p>
+					<a
+						href="https://en.wikipedia.org/wiki/Bob_Dylan"
+						className="btn btn-primary">
+						Go to wikipedia
+					</a>
+				</div>
+			</div>
+			<div className="card col-3">
+				<img
+					className="card-img-top"
+					src="https://github.com/4GeeksAcademy/react-tutorial-exercises/blob/master/.learn/assets/Dylan.png?raw=true"
+					alt="Card image cap"
+				/>
+				<div className="card-body">
+					<h5 className="card-title">hello</h5>
+					<p className="card-text">
+						Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is
+						an American singer-songwriter.
+					</p>
+					<a
+						href="https://en.wikipedia.org/wiki/Bob_Dylan"
+						className="btn btn-primary">
+						Go to wikipedia
+					</a>
+				</div>
 			</div>
 		</div>
 	);
--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Card from "./Card.jsx";
-
+import Jumbotron from "./Jumbotron.jsx";
 //create your first component
 const Home = () => {
 	return (
 		<div>
+			<Jumbotron />
 			<Card />
 		</div>
 	);
